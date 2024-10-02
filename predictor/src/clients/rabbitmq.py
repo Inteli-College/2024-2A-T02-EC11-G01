6ec4d461515241e80a0947266bac4c59cdc819c2
Henrique Marlon <henriquemarloncs@gmail.com>
2024-10-01 22:01:02 -0300

    fix: change event dispatcher dependency injection logic and entites strucuture to be compliant with migrations

--- predictor/src/clients/rabbitmq.py
+++ predictor/src/clients/rabbitmq.py
@@ -2,25 +2,21 @@ import pika
 from fastapi import Depends
 
 class RabbitMQClient:
-    def __init__(self, host: str, queue: str, username: str, password: str):
-        print(f"Connecting to RabbitMQ server at {host}")
+    def __init__(self, amqp_url: str):
+        print(f"Connecting to RabbitMQ server with URL {amqp_url}")
 
-        self.host = host
-        self.queue = queue
-        self.credentials = pika.PlainCredentials(username, password)
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.host, credentials=self.credentials)
-        )
+        self.connection_params = pika.URLParameters(amqp_url)
+        self.connection = pika.BlockingConnection(self.connection_params)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue, durable=True)
+        self.channel.queue_declare(queue='prediction', durable=True)
 
     def publish_message(self, message: str):
         self.channel.basic_publish(
             exchange='',
-            routing_key=self.queue,
+            routing_key='prediction',  # Use a fila apropriada
             body=message,
             properties=pika.BasicProperties(
-                delivery_mode=2,
+                delivery_mode=2,  # Persistência da mensagem
             )
         )
         print(f"Message published: {message}")
